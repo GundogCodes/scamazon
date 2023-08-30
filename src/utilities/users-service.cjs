2c8951d63dfebfd5c0e76736fc61925f0c6768eb
AndrewELe <125837056+AndrewELe@users.noreply.github.com>
2023-08-29 22:03:36 -0400

    utilities

--- src/utilities/users-service.cjs
+++ src/utilities/users-service.cjs
@@ -6,6 +6,28 @@ export async function signUp (userData) {
     return getUser()
 }
 
-export async function login(credentials) {
+export async function login (credentials) {
     const token = await usersAPI.login(credentials)
+    localStorage.setItem('token', token)
+    return getUser()
+}
+
+export function getToken() {
+    const token = localStorage.getItem('token')
+    if (!token) return null
+    const payload = JSON.parse(atob(token.split('.')[1]))
+    if (payload.exp < Date.now() / 1000) {
+        localStorage.removeItem('token')
+        return null
+    }
+    return token
+}
+
+export function getUser() {
+    const token = getToken()
+    return token ? JSON.parse(atob(token.split('.')[1])).user : null
+}
+
+export function logOut() {
+    localStorage.removeItem('token')
 }
