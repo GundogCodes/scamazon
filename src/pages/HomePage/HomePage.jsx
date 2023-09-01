97b07ae1e6d780b85cf4e1e846b12f7752fe500e
TJCole03 <tjc6254@gmail.com>
2023-08-31 22:55:31 -0400

    Final edits of the night. have blockage linking components with pages

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,23 +1,22 @@
 import styles from './HomePage.module.scss'
-import NavBar from '../componenets/NavBar'
-import PromoAdsSmall from '../components/PromoAdsSmall' 
-import PromoCarousel from '../components/PromoCarousel'
-import PromoCollection from '../comoponents/PromoCollection'
-import PromoMedia from '../components/PromoMedia'
-import PromoSeasonal from '../components/PromoSeasonal'
+import PromoAdsSmall from '../../components/PromoAdsSmall/PromoAdsSmall' 
+import PromoCarousel from '../../components/PromoCarousel/PromoCarousel'
+import PromoCollection from '../../comoponents/PromoCollection/PromoCollection'
+import PromoMedia from '../../components/PromoMedia/PromoMedia'
+import PromoSeasonal from '../../components/PromoSeasonal/PromoSeasonal'
 
 
 export default function HomePage(){
     return(
         <div className={styles.HomePage}>
             <img className="page-banner" src='../public/img/scamazon.png'/>
-            <h3>{NavBar}</h3>
+            <NavBar/>
             <h3>By, Alex, Andrew, Bao, Gunish, Jeff, Sapana, TJ</h3>
-            <h3>{PromoCarousel} promo carousel</h3>
-            <h3>{PromoAdsSmall} 3 smaller ads randomized</h3>
-            <h3>{PromoSeasonal} Seasonal Promo by date</h3>
-            <h3>{PromoMedia} Media </h3>
-            <h3>{PromoCollection} Collection Promotion</h3>
+            <PromoCarousel/>
+            <PromoAdsSmall/>
+            <PromoSeasonal/>
+            <PromoMedia/>
+            <PromoCollection/>
         </div>
     )
 }
