5f3439b7a1be06c427c4a5c402b9280fa73ceaf4
AndrewELe <125837056+AndrewELe@users.noreply.github.com>
2023-08-31 21:28:05 -0400

    playing around with the navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,12 +2,15 @@ import styles from './NavBar.module.scss'
 import {Link} from 'react-router-dom'
 export default function NavBar(props){
     return(
+
+      <div className="container">
         <nav className={styles.NavBarDiv}>
           {props.routes.map(({key,path})=>(
             <div className={styles.titles}>
-            <Link to={path} key={key} >{key}</Link>
+              <Link to={path} key={key} >{key}</Link>
             </div>
           ))}
         </nav>
-        )
+      </div>
+    )
 }
