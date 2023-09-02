6c06eb35baaffb7473e3d769e9515d72745d1d3e
AndrewELe <125837056+AndrewELe@users.noreply.github.com>
2023-09-02 13:43:11 -0400

    testing

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -1,10 +1,10 @@
 import styles from './OrderPage.module.scss'
 import {useState, UseEffect} from 'react'
 // import {Link} from 'react-router-dom'
-import * as ordersAPI from '../../utilities/orders-api'
+// import * as ordersAPI from '../../utilities/orders-api'
 // import Logo from '../../components/Logo/Logo'
 // import UserLogout from '../../components/LoginForm'
-import NavBar from '../../components/NavBar'
+// import NavBar from '../../components/NavBar'
 
 export default function OrderPage({user, setUser}) {
     //State
