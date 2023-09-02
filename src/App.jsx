e8add0ce3dd3e4549fcca32d18b2d65ff7a6c025
AndrewELe <125837056+AndrewELe@users.noreply.github.com>
2023-09-02 19:17:34 -0400

    placing category bar component

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,13 @@ import OrderPage from './pages/OrderPage/OrderPage'
 import SearchPage from './pages/SearchPage/SearchPage'
 import UserPage from './pages/UserPage/UserPage'
 import WishlistPage from './pages/WishlistPage/WishlistPage'
+
 function App() {
 
   return (
     <>
     <NavBar routes={routes} />
+    
     <Routes>
       <Route path="/" element={<HomePage />}/>
       <Route path="/login" element={<LoginPage />}/>
@@ -28,6 +30,8 @@ function App() {
       <Route path="/orders" element={<OrderPage />}/>
       <Route path="/wishlist" element={<WishlistPage />}/>
     </Routes>
+
+
     </>
   )
 }
