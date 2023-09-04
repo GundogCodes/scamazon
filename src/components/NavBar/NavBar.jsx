077d851d47430aa1b76084a36b0ff76e18396614
AndrewELe <125837056+AndrewELe@users.noreply.github.com>
2023-09-04 17:01:50 -0400

    made footer, still working on sidebar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,9 @@
 import styles from './NavBar.module.scss'
 import {Link} from 'react-router-dom'
-
+import CategoryBar from '../CategoryBar/CategoryBar'
 
 export default function NavBar(props){
     return(
-
       <div className="container">
         <nav className={styles.NavBarDiv}>
           {props.routes.map(({key,path})=>(
@@ -12,6 +11,9 @@ export default function NavBar(props){
               <Link to={path} key={key} >{key}</Link>
             </div>
           ))}
+
+          {/* placing category bar in the navbar (will modify to be a button) */}
+          <CategoryBar/>
         </nav>
       </div>
     )
