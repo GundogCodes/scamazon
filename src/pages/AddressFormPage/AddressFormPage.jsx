6cd435b79ec93e8e147de3d8012b4c570eac8931
Gunish <gunishsharma20@gmail.com>
2023-09-06 16:09:58 -0600

    conditionally redirecting addressformpage and edituserpage to loginpage based of whether there is a user logged in or not also added updateuser api call in usersAPI just need to connect the api to the front end

--- src/pages/AddressFormPage/AddressFormPage.jsx
+++ src/pages/AddressFormPage/AddressFormPage.jsx
@@ -1,7 +1,9 @@
 import {useState} from 'react'
+import {Link} from 'react-router-dom'
 import styles from './AddressFormPage.module.scss'
-export default function AddressForm({user,setUser}){
-    console.log('user, ',user)
+import LoginPage from '../LoginPage/LoginPage'
+export default function AddressForm({user, setUser}){
+
     const [address,setAddress] = useState({
         address:'',
         city:'',
@@ -9,43 +11,40 @@ export default function AddressForm({user,setUser}){
         zip:''
     })
 
-    // const user ={
-    //     name:"Gunish",
-    //     email:"gundog@gmail.com",
-    //     password:"Gun12345dog",
-    //     phoneNumber:'7805674567',
-    //     address:{
-    //         street:'123 st',
-    //         city:'Edmonton',
-    //         state:'Alberta',
-    //         zip:'6t94t0'
-    //     }
-    // }
-
+   
     return(
+        <div className={styles.AddressPage}>
+        {user?
         <form className={styles.AddressForm} >
             <div className={styles.currentAddress}>
-                <h1>Current Address</h1>
-                <p className={styles.username}>{user.name}</p>
-                <p className={styles.street}>{user.address.street}</p>
-                <p className={styles.city}>{user.address.city}</p>
-                <p className={styles.state}>{user.address.state}</p>
-                <p className={styles.zip}>{user.address.zip}</p>
-                <p className={styles.phone}>{user.phoneNumber}</p>
-
+            <h1>Current Address</h1>
+            <p className={styles.username}>{user.name}</p>
+            <p className={styles.street}>{/*user.address.street*/}</p>
+            <p className={styles.city}>{/*user.address.city*/}</p>
+            <p className={styles.state}>{/*user.address.state*/}</p>
+            <p className={styles.zip}>{/*user.address.zip*/}</p>
+            <p className={styles.phone}>{/*user.phoneNumber*/}</p>
+            
             </div>
             <div className={styles.theForm}>
             <h1>Add Address</h1>
-                <h3>
-                    <span>Default:</span> <img src='https://selenakitt.com/wp-content/files/scamazon.png'/>
-                </h3>
-
-            <p className={styles.text}>Address </p><input type='text' name="street" value={address.street}  className={styles.inputText} required />
-            <p className={styles.text}>City </p><input type='text' name="city" value={address.city}  className={styles.inputText} required />
-            <p className={styles.text}>State </p><input type='text' name="state" value={address.state}  className={styles.inputText} required />
-            <p className={styles.text}>Zip </p><input type='text' name="zip" value={address.zip}  className={styles.inputText} required />
+            <h3>
+            <span>Default:</span> <img src='https://selenakitt.com/wp-content/files/scamazon.png'/>
+            </h3>
+            
+            <p className={styles.text}>Address </p><input type='text' name="street"   className={styles.inputText} required />
+            <p className={styles.text}>City </p><input type='text' name="city"  className={styles.inputText} required />
+            <p className={styles.text}>State </p><input type='text' name="state"   className={styles.inputText} required />
+            <p className={styles.text}>Zip </p><input type='text' name="zip"   className={styles.inputText} required />
             <button type='submit'className={styles.submit} >Add Address</button>
             </div>
-        </form>
-    )
-}
+            </form>
+    :
+    <div className={styles.linkDiv}>
+    <h1 className={styles.linkToLogin}><Link to='/login'>Please Login to Continue </Link></h1>
+    </div>
+    }
+        </div>
+                
+            )
+        }
--- src/pages/EditUserPage/EditUserPage.jsx
+++ src/pages/EditUserPage/EditUserPage.jsx
@@ -1,8 +1,64 @@
 import styles from './EditUserPage.module.scss'
-import {useState} from 'react'
+import {useState, useRef} from 'react'
+import { updateUserInfo } from '../../utilities/users-api.cjs'
+// userId, newData
 export default function EditUserPage({user, setUser}){
     console.log('user ',user)
+    //console.log('user.id', user._id)
+    const [newName,setNewName] = useState()
+    const [newEmail,setNewEmail] = useState()
+    const [newPassword,setNewPassword] = useState()
+    const [newPhoneNumber,setNewPhoneNumber] = useState()
+
+ 
+
+    function handleChange(e){
+        console.log('typing field', e.target.name)
+        if(e.target.name ==='name'){
+            setNewName(e.target.value)
+        } else if(e.target.name === 'email'){
+            setNewEmail(e.target.value)
+        } else if(e.target.name === 'password'){
+            setNewPassword(e.target.value)
+        }else if(e.target.name === 'phoneNumber'){
+            setNewPhoneNumber(e.target.value)
+        }
+    }
+
+    function handleDoneButton(e){
+        const clickedButt = e.target.name
+        console.log('clickedButton ',e.target.name)
+        if(clickedButt === 'name'){
+            console.log(newName)
+        } else if(clickedButt ==='email'){
+            console.log(newEmail)
+            
+        }      
+        else if(clickedButt ==='password'){
+             console.log(newPassword)
+             
+            } else if(clickedButt ==='phoneNumber'){
+            console.log(newPhoneNumber)
+        }      
+            
+
+    }
+
+    const [userData,setUserData] = useState({
+        name:user.name || '',
+        email:user.email,
+        password:user.phoneNumber || '**********',
+        phoneNumber: user.phoneumber || ''
+    })
+
     const [showInput, setShowInput] = useState(false)
+
+     function handleSubmit(){
+
+        console.log('submitted')
+    }
+    
+
     function handleEditClick(){
         setShowInput(!showInput)
     }    
@@ -11,7 +67,7 @@ export default function EditUserPage({user, setUser}){
         <div className={styles.EditUserPage}>
             <h1>Login & Security</h1>
             <h2>Edit Info</h2>
-            <form >
+            <form onSubmit={handleSubmit} >
                 <div className={styles.editDiv}>
                     <div className={styles.heading}>
 
@@ -21,7 +77,10 @@ export default function EditUserPage({user, setUser}){
                 
                     </div>
                 {showInput?
-                <input type='text' name='name' required/>
+                <div className={styles.showInputDiv}>
+                <input  defaultValue={user.name}  type='text' name='name' onChange={handleChange}  required/>
+                {/* <button onClick={handleDoneButton} name='name'className={styles.doneButt}>Done</button> */}
+                </div>
                 :
                 <></>
             }
@@ -36,7 +95,10 @@ export default function EditUserPage({user, setUser}){
                 
                     </div>
                 {showInput?
-                <input type='text' name='email' required/>
+                <div className={styles.showInputDiv}>
+                <input defaultValue={user.email} type='text' name='email' onChange={handleChange} required/>
+                {/* <button onClick={handleDoneButton} name='email' className={styles.doneButt}>Done</button> */}
+                </div>
                 :
                 <></>
             }
@@ -45,13 +107,16 @@ export default function EditUserPage({user, setUser}){
                 <div className={styles.editDiv}>
                     <div className={styles.heading}>
 
-                <label>Password:</label>
-                <label>{user.password}</label>
+                <label>New Password:</label>
+                <label>{user.password||''}</label>
                 <button onClick={handleEditClick}>Edit</button>
                 
                     </div>
                 {showInput?
-                <input type='text' name='password' required/>
+                <div className={styles.showInputDiv}>
+                <input type='text' name='password' onChange={handleChange} required/>
+                {/* <button onClick={handleDoneButton} name='password' className={styles.doneButt}>Done</button> */}
+                </div>
                 :
                 <></>
             }
@@ -66,12 +131,15 @@ export default function EditUserPage({user, setUser}){
                 
                     </div>
                 {showInput?
-                <input type='text' name='phoneNumber' required/>
+                <div className={styles.showInputDiv}>
+                <input type='text' name='phoneNumber' onChange={handleChange}  />
+                {/* <button onClick={handleDoneButton} name='phoneNumber' className={styles.doneButt}>Done</button> */}
+                </div>
                 :
                 <></>
             }
                 </div>
-                <button type='submit'>Done</button>
+                <button  type='submit'>Submit Changes</button>
             </form>
         </div>
     )
