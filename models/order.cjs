a53ba0295929cc8a491331fd5f9379253f9453a9
Sapnapalkhade <sapnapalkhade@gmail.com>
2023-09-05 17:26:50 -0700

    changes for login, logout and sigup components and page

--- models/order.cjs
+++ models/order.cjs
@@ -107,7 +107,5 @@ orderSchema.methods.setItemQty = function(itemID, newQty) {
 }
 
 const Order = mongoose.model('Order', orderSchema)
-module.exports = Order
-const Order = mongoose.model('Order', orderSchema)
 
 module.exports = Order
--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,25 @@ import OrderPage from './pages/OrderPage/OrderPage'
 import SearchPage from './pages/SearchPage/SearchPage'
 import UserPage from './pages/UserPage/UserPage'
 import WishlistPage from './pages/WishlistPage/WishlistPage'
+import { getUser } from './utilities/users-service.cjs'
+import LogOut from './components/LogOut/LogOut'
 function App() {
+ const [user, setUser] = useState(getUser())
 
   return (
     <>
     <NavBar routes={routes} />
     <Routes>
       <Route path="/" element={<HomePage />}/>
-      <Route path="/login" element={<LoginPage />}/>
+      <Route path="/login" element={<LoginPage user={user} setUser={setUser}/>}/>
       <Route path="/user/:id" element={<UserPage />}/>
       <Route path="/search" element={<SearchPage />}/>
       <Route path="/item/:id" element={<ItemPage />}/>
       <Route path="/categories" element={<CategoryPage />}/>
       <Route path="/orders" element={<OrderPage />}/>
-      <Route path="/wishlist" element={<WishlistPage />}/>
+      <Route path="/wishlist" element={<WishlistPage user={user} setUser={setUser}/>}/>
     </Routes>
+    <LogOut user={user} setUser={setUser}/>
     </>
   )
 }
