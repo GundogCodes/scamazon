58d7e917fd474d12ee8a9229449baf7159480d8a
Alex <poyalex@gmail.com>
2023-09-09 11:16:01 -0400

    wishlist Merge

---
+++ controllers/wishList.cjs
@@ -0,0 +1,33 @@
+const WishList = require('../models/wishList.cjs');
+const User = require('../models/user.cjs');
+
+module.exports = {
+  wishList,
+  //create wishList,
+  addToWishList,
+  //   removeItemFromWishList,
+};
+
+// A cart is the unpaid order for a user
+async function wishList(req, res) {
+  try {
+    console.log('req.user._id: ', req.user._id);
+    const wishList = await WishList.getWishList(req.user._id);
+    res.status(200).json(wishList);
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
+  }
+}
+
+//Add an item to the cart
+async function addToWishList(req, res) {
+  try {
+    const wishList = await WishList.getWishList(req.user._id);
+    wishList.user = await User.findById(req.user._id);
+    await wishList.addItem(req.params.id);
+    res.status(200).json(wishList);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({ msg: e.message });
+  }
+}
