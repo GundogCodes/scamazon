0b9b35597468f5a27de80478f09a603d93300000
Alex <poyalex@gmail.com>
2023-09-11 01:03:31 -0400

    Added error handling for wishlist render

--- src/pages/WishlistPage/WishlistPage.jsx
+++ src/pages/WishlistPage/WishlistPage.jsx
@@ -1,16 +1,15 @@
 import { useState, useEffect } from 'react';
 import styles from './WishlistPage.module.scss'
 import { getWishList, addToWishList, removeItem } from '../../utilities/wishList-api.cjs';
-import * as addToCart from '../ItemPage/Itempage.cjs'
-import * as itemsAPI from '../../utilities/items-api.cjs'
+// import * as addToCart from '../ItemPage/Itempage.jsx'
+import { addToCart, getCart } from '../../utilities/orders-api.cjs'
 import LoginPage from '../LoginPage/LoginPage'
 import {Link} from 'react-router-dom'
-//Import cart
 
 export default function WishlistPage({user}) {
   const [userWishlist, setUserWishlist] = useState ([]) 
-  const items = userWishlist.items
-  // const wishlistItemContainer = document.querySelector(".container")
+  
+
   
   useEffect(() => { 
     (async () => {
@@ -21,20 +20,22 @@ export default function WishlistPage({user}) {
   })
   // console.log(userWishlist)
   //Image, name, price
-    const wishlistItems = items.map(wishlistItem => {
-      <div key={wishlistItem._id}>
-        <div classname={styles.wishlistItem}>
-          <link to={`/item/${wishlistItem.item._id}`}>
-            <img src={wishlistItem.item.image} className={styles.image} alt={wishlistItem.item.name} />
-          </link>
-          <p>Name: {wishlistItem.item.name}</p>
-          <p>Price: ${wishlistItemitem.price.toFixed(2)}</p>
-          <button onClick={() => removeItem(wishlistItem._id)}>Remove Item</button>
-          <button onClick={() => handleAddToCart(wishlistItem._id, wishlistItem.delete)}>Add to Cart</button>
-        </div>
+  const items = userWishlist.items
+  const wishlistItems = items.map(wishlistItem => {
+    <div key={wishlistItem._id}>
+      <div classname={styles.wishlistItem}>
+        <link to={`/item/${wishlistItem.item._id}`}>
+          <img src={wishlistItem.item.image} className={styles.image} alt={wishlistItem.item.name} />
+        </link>
+        <p>Name: {wishlistItem.item.name}</p>
+        <p>Price: ${wishlistItemitem.price.toFixed(2)}</p>
+        <button onClick={() => removeItem(wishlistItem._id)}>Remove Item</button>
+        <button onClick={() => addToCart(wishlistItem._id, wishlistItem.delete)}>Add to Cart</button>
       </div>
-    })
+    </div>
+  })
 
+  // const wishlistItemContainer = document.querySelector(".container")
   // function ShowWishlistItems() {
   //   items.map ((Item) => {
   //     return(
@@ -62,12 +63,16 @@ export default function WishlistPage({user}) {
       <>
       <h1>{user.name}'s Wishlist</h1>
       <div id= "container" className={styles.WishlistContainer}>
-        {wishlistItems}
+        {!userWishlist || !items || items.length === 0 ? 
+          (<div>Wishlist is empty :(</div>)
+          :
+          ({wishlistItems})
+        }
       </div>
       </>
       :
-      <h1>Test</h1>
-      /* <LoginPage user={user} setUser={setUser}/> */
+      // <h1>Test</h1>
+      <LoginPage user={user} setUser={setUser}/>
       }
     </div>
   );
