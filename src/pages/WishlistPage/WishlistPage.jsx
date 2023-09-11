475df66fe586e1d431499cffeff69f68f28d19d1
Alex <poyalex@gmail.com>
2023-09-11 00:29:38 -0400

    Completed WishlistPage Frontend w/basic styling. Testing Needed

--- src/pages/WishlistPage/WishlistPage.jsx
+++ src/pages/WishlistPage/WishlistPage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import styles from './WishlistPage.module.scss'
 import { getWishList, addToWishList, removeItem } from '../../utilities/wishList-api.cjs';
+import * as addToCart from '../ItemPage/Itempage.cjs'
 import * as itemsAPI from '../../utilities/items-api.cjs'
 import LoginPage from '../LoginPage/LoginPage'
 import {Link} from 'react-router-dom'
@@ -9,7 +10,6 @@ import {Link} from 'react-router-dom'
 export default function WishlistPage({user}) {
   const [userWishlist, setUserWishlist] = useState ([]) 
   const items = userWishlist.items
-
   // const wishlistItemContainer = document.querySelector(".container")
   
   useEffect(() => { 
@@ -20,8 +20,21 @@ export default function WishlistPage({user}) {
     ()
   })
   // console.log(userWishlist)
-  //userWishlist.items
   //Image, name, price
+    const wishlistItems = items.map(wishlistItem => {
+      <div key={wishlistItem._id}>
+        <div classname={styles.wishlistItem}>
+          <link to={`/item/${wishlistItem.item._id}`}>
+            <img src={wishlistItem.item.image} className={styles.image} alt={wishlistItem.item.name} />
+          </link>
+          <p>Name: {wishlistItem.item.name}</p>
+          <p>Price: ${wishlistItemitem.price.toFixed(2)}</p>
+          <button onClick={() => removeItem(wishlistItem._id)}>Remove Item</button>
+          <button onClick={() => handleAddToCart(wishlistItem._id, wishlistItem.delete)}>Add to Cart</button>
+        </div>
+      </div>
+    })
+
   // function ShowWishlistItems() {
   //   items.map ((Item) => {
   //     return(
@@ -49,7 +62,7 @@ export default function WishlistPage({user}) {
       <>
       <h1>{user.name}'s Wishlist</h1>
       <div id= "container" className={styles.WishlistContainer}>
-        <ShowWishlistItems/>
+        {wishlistItems}
       </div>
       </>
       :
